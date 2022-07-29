7ab2087f59a6182577aa2afd7fafb37596c3c64f
Andrew-Sem <avvyayvayva@gmail.com>
2022-07-29 17:48:54 +0300

    remove tap highlight for *

--- components/CourseCategory.tsx
+++ components/CourseCategory.tsx
@@ -7,7 +7,7 @@ interface CourseCategoryProps {
 }
 
 const CourseCategory: FC<CourseCategoryProps> = ({title, active, onClick}) => {
-    let rootClasses = "w-full md:w-4/5 bg-gray-100 p-3 rounded cursor-pointer hover:bg-yellow-300 easy-in duration-200"
+    let rootClasses = "w-full md:w-4/5 bg-gray-100 p-3 rounded cursor-pointer hover:bg-yellow-300 easy-in duration-200 outline:none"
     if (active) rootClasses += " bg-yellow-300"
 
     return (
