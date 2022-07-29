e253563d98222b8a628588eadd6cd080bd17a966
Andrew-Sem <avvyayvayva@gmail.com>
2022-07-29 17:18:39 +0300

    fix button in smallscreens

--- components/UI/MainButton.tsx
+++ components/UI/MainButton.tsx
@@ -9,7 +9,7 @@ interface MoreLinkProps {
 const MainButton: FC<MoreLinkProps> = ({href, title}) => {
     return (
         <Link href={href}>
-            <a className={"p-5 bg-yellow-300 rounded-xl mt-4 w-full md:w-40 hover:bg-white border-solid border-yellow-300 border ease-in duration-200 inline-block cursor-pointer flex items-center justify-center"}>
+            <a className={"p-5 bg-yellow-300 rounded-xl my-4 w-full md:w-40 hover:bg-white border-solid border-yellow-300 border ease-in duration-200 inline-block cursor-pointer flex items-center justify-center"}>
                 {title}
             </a>
         </Link>
